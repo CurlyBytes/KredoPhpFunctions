bfe4bde572e204d29d2d431c497e3f4e91ddd12c
cocoytech <francisco.abayon@rafi.ph>
2020-09-17 00:26:27 +0800

    Feat(codebase): add loan disbursement and payment process

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-    echo  str_replace("MYNAME", "Cocoy", "Hello i am MYNAME.\n");
-    echo  str_replace("MYNAME", "Cocoy", "Hi MYNAME, im Kredo Virtual Assistant Bot, How may i help you?");
+//A new start FinTech that focus on microfinance, the organization just want to have automate their process on loan disbursement and loan payment.
+$loan = 5000;
+$serviceChargeAmount = 500;
+$interestPercentageRate = 0.05;
+$termsOfPayment = 6;
+
+//loan payment amount
+$loanToPaymentAmount = ($loan + ($loan * $interestPercentageRate));
+$amountToPayBaseOnTerm = $loanToPaymentAmount / $termsOfPayment  ;
+
+echo "The total amount to pay is $loanToPaymentAmount. \nBased on the Contact your going to pay $amountToPayBaseOnTerm by $termsOfPayment times";
 ?>
