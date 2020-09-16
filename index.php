a57661a723c8e6bc7f3429696e80f91c1cd27845
cocoytech <francisco.abayon@rafi.ph>
2020-09-17 00:38:45 +0800

    Refactor(codebase): add function on loan disbursement

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 
 //A new start FinTech that focus on microfinance, the organization just want to have automate their process on loan disbursement and loan payment.
 
-// How about payment type? daily, weekly, monthly , quarterly or yearly?
-// How about user will have multiple loans?
-// The interest perecentage will dynamically change base on loan amount?
-$loan = 5000;
-$serviceChargeAmount = 500;
-$interestPercentageRate = 0.05;
-$termsOfPayment = 6;
 
-//loan payment amount
-$loanToPaymentAmount = ($loan + ($loan * $interestPercentageRate));
-$amountToPayBaseOnTerm = $loanToPaymentAmount / $termsOfPayment  ;
+function LoanDisbursement($loan, 
+                $termsOfPayment, 
+                $serviceChargeAmount = 500, 
+                $interestPercentageRate = 0.05 ){
 
-echo "The total amount to pay is $loanToPaymentAmount. \nBased on the Contact your going to pay $amountToPayBaseOnTerm by $termsOfPayment times";
+    //loan payment amount
+    $loanToPaymentAmount = $loan + ($loan * $interestPercentageRate);
+    $amountToPayBaseOnTerm = $loanToPaymentAmount / $termsOfPayment  ;
+
+    echo "\nThe total amount to pay is $loanToPaymentAmount. \nBased on the Contact your going to pay $amountToPayBaseOnTerm by $termsOfPayment times\n";
+}
+
+
+LoanDisbursement(5000, 6);
+LoanDisbursement(10000, 6, 300, 0.2);
+LoanDisbursement(15000, 12 , 1000 , 0.07);
 ?>
