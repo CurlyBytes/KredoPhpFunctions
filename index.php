13ffe3fc8a9deb3dd264b32c4ef19c4a97dbad93
Restyled.io <commits@restyled.io>
2020-09-16 15:51:55 +0000

    Restyled by php-cs-fixer

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    function addFunction($num1, $num2) {
-    $sum = $num1 + $num2;
-    echo "Sum of the two numbers is : $sum \n";
+    function addFunction($num1, $num2)
+    {
+        $sum = $num1 + $num2;
+        echo "Sum of the two numbers is : $sum \n";
     }
     
     addFunction(10, 20);
     addFunction(16, 33);
     addFunction(831, 0);
     addFunction(3, 8);
-?>
