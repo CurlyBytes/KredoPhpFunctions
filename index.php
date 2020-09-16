6106f1fd87b7e6b74cc472d90d5cb320a0d37187
cocoytech <francisco.abayon@rafi.ph>
2020-09-17 00:43:14 +0800

    Fix(codebase): fix negative or zero in number of loan payment

--- index.php
+++ index.php
@@ -8,15 +8,25 @@ function LoanDisbursement($loan,
                 $serviceChargeAmount = 500, 
                 $interestPercentageRate = 0.05 ){
 
-    //loan payment amount
-    $loanToPaymentAmount = $loan + ($loan * $interestPercentageRate);
-    $amountToPayBaseOnTerm = $loanToPaymentAmount / $termsOfPayment  ;
 
-    echo "\nThe total amount to pay is $loanToPaymentAmount. \nBased on the Contact your going to pay $amountToPayBaseOnTerm by $termsOfPayment times\n";
+
+    if($termsOfPayment <= 0) {
+
+        echo "Loan is not applicable, please supply Number of Terms. \n";
+    }
+    else{
+        //loan payment amount
+        $loanToPaymentAmount = $loan + ($loan * $interestPercentageRate);
+        $amountToPayBaseOnTerm = $loanToPaymentAmount / $termsOfPayment ;
+
+        echo "\nThe total amount to pay is $loanToPaymentAmount. \nBased on the Contact your going to pay $amountToPayBaseOnTerm by $termsOfPayment times\n";
+
+    }
+
 }
 
 
-LoanDisbursement(5000, 6);
+LoanDisbursement(5000, 0);
 LoanDisbursement(10000, 6, 300, 0.2);
 LoanDisbursement(15000, 12 , 1000 , 0.07);
 ?>
